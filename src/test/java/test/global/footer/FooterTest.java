061834dc328050edb20c7e0ed609b48f4e1dbcdc
Minh Nguyen <msminhnt@gmail.com>
2022-08-07 00:16:43 +0700

    lesson 21, Test flow initial

--- src/test/java/test/global/footer/FooterTest.java
+++ src/test/java/test/global/footer/FooterTest.java
@@ -11,11 +11,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import support.verification.Verifier;
+import test_flows.global.FooterTestFlow;
 import url.Urls;
 
 public class FooterTest {
     @Test(priority = 1, dependsOnMethods = {"testFooterRegisterPage"})
     public void testFooterCategoryPage() {
+        WebDriver driver = DriverFactory.getChromeDriver();
+        driver.get(Urls.demoBaseUrl);
+        FooterTestFlow footerTestFlow= new FooterTestFlow(driver);
+        footerTestFlow.verifyFooterComponent();
     }
 
     @Test(priority = 2)
---
+++ src/test/java/test_flows/global/FooterTestFlow.java
@@ -0,0 +1,37 @@
+package test_flows.global;
+
+import models.components.global.footer.FooterColumnComponent;
+import org.openqa.selenium.WebDriver;
+
+public class FooterTestFlow {
+
+    private final WebDriver driver;
+
+    public FooterTestFlow(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyFooterComponent() {
+        verifyInformationColumn();
+        verifyCustomerServiceColumn();
+        verifyAccountColumn();
+        verifyFollowUsColumn();
+
+    }
+
+    private void verifyInformationColumn() {
+    }
+    private void verifyCustomerServiceColumn() {
+    }
+    private void verifyAccountColumn() {
+    }
+    private void verifyFollowUsColumn() {
+    }
+    private static void testFooterColumn(FooterColumnComponent footerColumnComponent) {
+        System.out.println(footerColumnComponent.headElem().getText());
+        footerColumnComponent.linksElem().forEach(link -> {
+            System.out.println(link.getText());
+            System.out.println(link.getAttribute("href"));
+        });
+    }
+}
